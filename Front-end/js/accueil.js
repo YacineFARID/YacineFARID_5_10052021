77607936ad083fb88e72076eed42dcf3d27327b0
YacineFARID <yacine.farid.pgp@gmail.com>
2021-05-26 06:19:42 +0200

    ajout page product

--- Front-end/js/accueil.js
+++ Front-end/js/accueil.js
@@ -1,19 +1,40 @@
 console.log("accueil");
 
+
 const getProductsFromApi = async () => {
 
-	return await fetch("http://localhost:3000/api/cameras/")
-		.then((response) => response.json())
-		.catch((error) => new Error(error));
+    return await fetch("http://localhost:3000/api/cameras/")
+    .then((response) => response.json())
+	.catch((error) => new Error(error));
 };
 
 const displayProduct = async () => {
     const container = document.getElementById("camera")
-    const data = await getProductsFromApi();
-    data.forEach(element => console.log(element));
+   const data = await getProductsFromApi();
+   data.forEach(element => console.log(element));
 
 }
 
- displayProduct();
+displayProduct();
+
+fetch("http://localhost:3000/api/cameras/")
+.then((response) => response.json())
+.then(cameras => {
+    cameras.forEach(camera => {
+    //Affichage des produits
+
+    const h2 = document.createElement("h2");
+    h2.textContent = camera.name;
+    const p = document.createElement("p");
+    p.textContent = camera.price / 100 + "€";
+    const img = document.createElement("img");
+    img.src = camera.imageUrl;
+    const h3 = document.createElement("h3");
+    h3.textContent = camera.description;
+    const a = document.createElement("a");
+    a.hreft = '/Front-end/'
+    }
+    
+}
 
  
